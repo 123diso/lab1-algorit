12467c01cb4556300364a106963b36bde3a7be41
unknown <d.f9821n@gmail.com>
2025-03-13 16:42:48 -0500

    Añadiendo el index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Cartas from './components/card/Cartas.js'
+import Menu from './components/nav/Menu.js';
+
+customElements.define("add-cart", Cartas);
+customElements.define("menu-component", Menu);
