a802b492b07c53d7c711eaf36ec13cc83d7e7947
Sofi <sofiadeaza05@gmail.com>
2025-03-14 12:44:05 -0500

    agg fooder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Cartas from './components/card/Cartas.js'
 import Menu from './components/nav/Menu.js';
 import './components/Comentarios/CompComent.js';
 import './components/paginacion/paginacion.js';
-
+import './components/footer/Footer.js';
 
 customElements.define("add-cart", Cartas);
 customElements.define("menu-component", Menu);
